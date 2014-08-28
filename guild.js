60f8f40fddd92633e793cf0c000ca7be0f6c78f3
villep <ville.pynttari@ambientia.fi>
2014-08-28 21:37:08 +0300

    Subguild selection

--- guild.js
+++ guild.js
@@ -11,16 +11,9 @@ app.controller("GuildController", function ($scope, $http) {
     });
 
     $scope.availableLevels = function (guild) {
-        var availableLevels = 45;
 
-        var totalLevels = 0
-        totalLevels += $scope.reinc.freeLevels
-        var chosenGuildsListLength = $scope.reinc.guilds.length;
-        for (var i = 0; i < chosenGuildsListLength; i++) {
-            totalLevels += $scope.reinc.guilds[i].chosenLevels;
-        }
-
-        availableLevels = 120 - totalLevels;
+        var totalLevels = getChosenLevelsAmount();
+        var availableLevels = 120 - totalLevels;
 
         if (guild.levels < availableLevels)
             availableLevels = guild.levels;
@@ -38,29 +31,44 @@ app.controller("GuildController", function ($scope, $http) {
     };
 
     $scope.updateFreeLevels = function () {
-        var totalLevels = 0
-        var freeLevels = $scope.reinc.freeLevels
-        totalLevels += freeLevels
-        var chosenGuildsListLength = $scope.reinc.guilds.length;
-        for (var i = 0; i < chosenGuildsListLength; i++) {
-            totalLevels += $scope.reinc.guilds[i].chosenLevels;
-        }
-        if (freeLevels > 0 && totalLevels > 120)
+        var totalLevels = getChosenLevelsAmount();
+        if ($scope.reinc.freeLevels > 0 && totalLevels > 120)
             $scope.reinc.freeLevels = 0
-
     }
 
-
-    $scope.updateGuild = function (guild) {
-        var totalLevels = 0
-        totalLevels += $scope.reinc.freeLevels
+    function getChosenLevelsAmount() {
+        var levels = 0;
+        levels += $scope.reinc.freeLevels
         var chosenGuildsListLength = $scope.reinc.guilds.length;
         for (var i = 0; i < chosenGuildsListLength; i++) {
-            totalLevels += $scope.reinc.guilds[i].chosenLevels;
+            levels += $scope.reinc.guilds[i].chosenLevels;
+            for (var j = 0; j < $scope.reinc.guilds[i].subguilds.length; j++) {
+                if ($scope.reinc.guilds[i].subguilds[j].chosenLevels > 0)
+                    levels += $scope.reinc.guilds[i].subguilds[j].chosenLevels;
+            }
+        }
+        console.log(levels);
+        return levels;
+    }
+
+    function removeSubguildsFromGuild(guild) {
+        guild.subguilds = [];
+        for (var i = 0; i < $scope.reinc.guilds.length; i++) {
+            if (guild.name == $scope.reinc.guilds[i].name) {
+                for (var j = 0; j < $scope.reinc.guilds[i].subguilds.length; j++)
+                    $scope.reinc.guilds[i].subguilds = [];
+            }
+        }
+    }
+
+    $scope.updateGuild = function (guild) {
+        var totalLevels = getChosenLevelsAmount();
+        var chosenLevels = guild.chosenLevels;
+
+        if (chosenLevels < 45 && guild.subguilds != null) {
+            removeSubguildsFromGuild(guild);
         }
 
-
-        var chosenLevels = guild.chosenLevels
         if (chosenLevels > 0) {
 
             var exists = false;
@@ -73,10 +81,9 @@ app.controller("GuildController", function ($scope, $http) {
                     guild.chosenLevels = "";
                     return;
                 }
-                $scope.reinc.guilds.push({
-                    name: guild.name,
-                    chosenLevels: chosenLevels
-                });
+                getSubguilds(guild);
+                $scope.reinc.guilds.push(guild);
+
             }
         }
 
@@ -91,6 +98,7 @@ app.controller("GuildController", function ($scope, $http) {
                 }
             }
             if (exists == true) {
+                removeSubguildsFromGuild(guild)
                 $scope.reinc.guilds.splice(itemIndex, 1);
             }
         }
@@ -99,7 +107,6 @@ app.controller("GuildController", function ($scope, $http) {
     function getGuilds() {
         $http({method: 'GET', url: '//api.github.com/repos/pynttvi/char-creator-data/contents/guilds.chr'}).
             success(function (data, status, headers, config) {
-
                 var guild = atob(data.content);
                 var lines = guild.split('\n');
                 for (var i = 0; i < lines.length; i++) {
@@ -107,7 +114,9 @@ app.controller("GuildController", function ($scope, $http) {
                     $scope.guilds.push({
                         id: i,
                         name: cols[0],
-                        levels: cols[1]
+                        levels: cols[1],
+                        chosenLevels: "",
+                        subguilds: []
                     });
                 }
             }).
@@ -117,5 +126,44 @@ app.controller("GuildController", function ($scope, $http) {
             });
     }
 
+    function getSubguilds(guild) {
+        if (guild.chosenLevels == 45 && guild.subguilds.length == 0) {
+            var guildFileName = guild.name.replace(" ", "-").toLowerCase() + ".chr"
+            $http({method: 'GET', url: '//api.github.com/repos/pynttvi/char-creator-data/contents/' + guildFileName}).
+                success(function (data, status, headers, config) {
+
+                    var guildFile = atob(data.content);
+                    var lines = guildFile.split('\n');
+                    var subGuildsStartIndex
+                    for (var i = 0; i < lines.length; i++) {
+                        if (lines[i].contains("Subguilds:") == true)
+                            subGuildsStartIndex = i;
+
+                        if (subGuildsStartIndex != null && subGuildsStartIndex < i && lines[i] != "") {
+                            var cols = lines[i].split(" ");
+                            var exists = false;
+                            for (var j = 0; j < guild.subguilds.length; j++) {
+                                if (cols[0] == guild.subguilds[j].name) {
+                                    exists = true;
+                                }
+                            }
+                            if (exists == false) {
+                                guild.subguilds.push({
+                                    name: cols[0],
+                                    levels: cols[1]
+                                });
+                            }
+                        }
+                    }
+                }).
+                error(function (data, status, headers, config) {
+                    alert("error:" + status);
+                    alert("error:" + data);
+                });
+        }
+        else {
+            removeSubguildsFromGuild(guild);
+        }
+    }
 });
 
